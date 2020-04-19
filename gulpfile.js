8b76f5556296d0e2488da13818cbbfcb47de119b
Jackson Weber <weberjackson79@gmail.com>
2020-04-18 22:12:15 -0400

    Add gulp configuration.

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const { series, parallel, watch } = require('gulp');
+const gulp = require('gulp');
+const gls = require('gulp-live-server');
+const ts = require('gulp-typescript');
+const del = require("del");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const tsify = require("tsify");
+
+let server = null;
+
+
+const buildBackend = () => {
+    const tsProject = ts.createProject('tsconfig.json');
+    const tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    if (!server) {
+        server = gls.new('./dist/index.js');
+    }
+
+    return tsResult.js.pipe(gulp.dest('dist'));
+};
+
+const buildFrontend = () => {
+    return browserify({
+        basedir: ".",
+        debug: true,
+        entries: ["src/views/FrontendEntry.ts"],
+        cache: {},
+        packageCache: {}
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source("bundle.js"))
+        .pipe(gulp.dest("dist-frontend"));
+};
+
+const restartServer = (cb) => {
+    server.start();
+};
+
+const watchDir = () => {
+    watch(['./src/**/*.ts'], series(
+        build,
+        restartServer
+    ));
+};
+
+const clean = () => {
+    return del(["./dist/"]);
+};
+
+const build = parallel(buildBackend, buildFrontend);
+
+exports.default = series(build, parallel(watchDir, restartServer));
+exports.build = build;
+exports.clean = clean;
